871a914ba0f52553fb27e39c097fda03d71ef142
Kostiantyn Danylov aka koder <koder.mail@gmail.com>
2015-01-29 23:41:27 +0200

    pythonifications

--- make_report.py
+++ make_report.py
@@ -1,26 +1,22 @@
+import json
 import gspread
 import argparse
-import json
+
 
 from config import DEFAULT_FILE_PATH, PASSWORD, EMAIL, COL_COUNT, WORK_SHEET, DOCUMENT_ID
 
 
 def load_data(file_name):
-    with open(file_name) as f:
-        data = f.read()
-        return json.loads(data)
+    return json.loads(open(file_name).read())
 
-
-def make_report(data):
+def insert_data(data):
     gc = gspread.login(EMAIL, PASSWORD)
     sh = gc.open_by_key(DOCUMENT_ID)
     worksheet = sh.add_worksheet(title=WORK_SHEET, rows=len(data.keys()), cols=COL_COUNT)
 
-    i = 1
-    for k in data.keys():
+    for i, (k, v) in enumerate(data.items(), 1):
         worksheet.update_cell(i, 1, k)
-        worksheet.update_cell(i, 2, data[k])
-        i += 1
+        worksheet.update_cell(i, 2, v)
 
 
 if __name__ == '__main__':
@@ -28,4 +24,4 @@ if __name__ == '__main__':
     parser.add_argument('-n', '--name', help='data file path', default=DEFAULT_FILE_PATH)
     results = parser.parse_args()
     data = load_data(results.name)
-    make_report(data)
+    insert_data(data)
