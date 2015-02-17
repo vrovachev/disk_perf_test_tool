61e698abf127d33d1a63b212bc38638b37514385
gstepanov <gstepanov@mirantis.com>
2015-02-17 19:11:59 +0200

    foreign keys added

--- web_app/models.py
+++ web_app/models.py
@@ -20,8 +20,9 @@ class Param(db.Model):
 
 class ParamCombination(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # param_id = db.Column(db.Integer, ForeignKey('param.id'))
-    # param = relationship("param")
+    param_1 = db.Column(db.Integer, ForeignKey('param.id'))
+    param_2 = db.Column(db.Integer, ForeignKey('param.id'))
+    param_3 = db.Column(db.Integer, ForeignKey('param.id'))
 
 
 class Lab(db.Model):
@@ -35,7 +36,7 @@ class Result(db.Model):
     build_id = db.Column(db.Integer)
     lab_id = db.Column(db.Integer)
     time = db.Column(db.DateTime)
-    # param_combination_id = db.Column(db.Integer, ForeignKey('param_combination.id'))
-    # param_combination = relationship("param_combination")
+    param_combination_id = db.Column(db.Integer, ForeignKey('param_combination.id'))
+    param_combination = relationship("param_combination")
     bandwith = db.Column(db.Float)
     meta = db.Column(db.String(4096))
